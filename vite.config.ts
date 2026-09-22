9474f48d3754f00aaa4ef1df0737f8d61074e202
Muriel Messias <155545873+MurielMessias@users.noreply.github.com>
2026-09-22 12:39:16 -0300

    fref

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { fileURLToPath, URL } from 'node:url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/Ominify-LandingPage/' : '/',
+  base: '/Ominify-LandingPage/',
   plugins: [react()],
   resolve: {
     alias: {
